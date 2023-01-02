694603beeabc9d4dd8107dda5af2cba926346deb
MarcoAbiusi <89129318+MarcoAbiusi10@users.noreply.github.com>
2023-01-02 22:40:53 +0100

    Aggiustamenti pagina profilo e aggiunta della toolbar

--- app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
@@ -1,5 +1,6 @@
 package it.uniba.dib.sms222332;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -11,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -29,6 +33,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView settingsIcon = findViewById(R.id.icon_settings);
+        ImageView profileIcon = findViewById(R.id.icon_profile);
+
+
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ProfileFragment.class));
+            }
+        });
+
         NavigationBarView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
@@ -36,23 +51,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private final NavigationBarView.OnItemSelectedListener navListener = item -> {
-        Fragment selectedFragment;
+            Fragment selectedFragment;
 
-        switch (item.getItemId()){
+            switch (item.getItemId()) {
 
-            case R.id.star_button:
-                selectedFragment = new FavoritesFragment();
-                break;
+                case R.id.star_button:
+                    selectedFragment = new FavoritesFragment();
+                    break;
 
-            case R.id.chat_button:
-                selectedFragment = new MessagesFragment();
-                break;
+                case R.id.chat_button:
+                    selectedFragment = new MessagesFragment();
+                    break;
 
-            default:
-                selectedFragment = new HomeFragment();
-        }
+                default:
+                    selectedFragment = new HomeFragment();
+            }
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
         return true;
     };
+
+
 }
