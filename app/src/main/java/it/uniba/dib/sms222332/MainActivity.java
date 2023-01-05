2e8c161af3e37f228f82e0c7810f297086773e84
MarcoAbiusi <89129318+MarcoAbiusi10@users.noreply.github.com>
2023-01-05 19:03:29 +0100

    Aggiunta animazione per impostazioni

--- app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private final NavigationBarView.OnItemSelectedListener navListener = item -> {
             Fragment selectedFragment;
 
