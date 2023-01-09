9a81571aa871504beb0adf89546a3a6d8e4b9b1f
MarcoAbiusi <89129318+MarcoAbiusi10@users.noreply.github.com>
2023-01-09 22:33:37 +0100

    Inserito il Drawer Layout

---
+++ app/src/main/java/it/uniba/dib/sms222332/ui/profile/ProfileFragment.java
@@ -0,0 +1,36 @@
+package it.uniba.dib.sms222332.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import it.uniba.dib.sms222332.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
