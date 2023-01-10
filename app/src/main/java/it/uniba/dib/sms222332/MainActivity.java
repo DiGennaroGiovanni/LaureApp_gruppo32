5a938506a7274ed6276fb9ade9df466635a09eab
Beppe Giacovelli <beppegiacovelli@gmail.com>
2023-01-10 19:28:15 +0100

    uniti branch beppe e marcuzz + aggiustamenti

--- app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
@@ -1,18 +1,15 @@
 package it.uniba.dib.sms222332;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
 
 import it.uniba.dib.sms222332.databinding.ActivityMainBinding;
 
