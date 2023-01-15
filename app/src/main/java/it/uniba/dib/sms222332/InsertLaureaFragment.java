0287711c782ae9a481e4e9fffab11031f591db0d
ProgettoSysAg <sysag2022@gmail.com>
2023-01-15 20:27:56 +0100

    Aggiunta funzione per la selezione del correlatore

--- app/src/main/java/it/uniba/dib/sms222332/InsertLaureaFragment.java
+++ app/src/main/java/it/uniba/dib/sms222332/InsertLaureaFragment.java
@@ -1,6 +1,7 @@
 package it.uniba.dib.sms222332;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -11,9 +12,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,11 +26,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -40,9 +49,9 @@ import java.util.Map;
 
 public class InsertLaureaFragment extends Fragment {
 
-    EditText edtThesisName, edtEstimatedTime, edtCorrelator,edtDescription, edtThesisConstraints,edtRelatedProjects;
+    EditText edtThesisName, edtEstimatedTime,edtDescription, edtThesisConstraints,edtRelatedProjects;
     RadioButton radioButtonSperimentale,radioButtonCompilativa;
-    Spinner edtMainSubject;
+    Spinner edtMainSubject,edtCorrelator;
     Button addFile,buttonCreateThesis;
     TextView textView6;
 
@@ -55,17 +64,14 @@ public class InsertLaureaFragment extends Fragment {
     Uri pdfUri;
     LinearLayout layout;
 
-    ArrayList<Uri> filePdf ;
+    ArrayList<Uri> filePdf = new ArrayList<>() ;
+    ArrayList<String> correlatori = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_insert_laurea, container, false);
 
-        /*View primoLayout = view.findViewById(R.id.first_layout);
-        View secondoLayout = primoLayout.findViewById(R.id.second_layout);
-        View terzoLayout = primoLayout.findViewById(R.id.third_layout);
-       */
         layout = view.findViewById(R.id.layout_lista);
 
         textView6 = view.findViewById(R.id.textView6);
@@ -87,12 +93,12 @@ public class InsertLaureaFragment extends Fragment {
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
-        filePdf = new ArrayList<>();
         /*      GESTISCO LO SPINNER     */
         // Creo un ArrayAdapter che contiene i valori dello spinner e gli affiso il layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.faculty_array, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.faculty_array, android.R.layout.simple_spinner_item);
         // Specifico il layout che appare quando viene cliccato sullo spinner
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Setto l'ArrayAdapter allo spinner
         edtMainSubject.setAdapter(adapter);
@@ -115,6 +121,28 @@ public class InsertLaureaFragment extends Fragment {
             }
         });
 
+        CollectionReference collectionRef = db.collection("professori");
+
+        collectionRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String nome = document.getString("Nome")+" "+ document.getString("Cognome");
+                        correlatori.add(nome);
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+        correlatori.add("Nessuno");
+
+        ArrayAdapter<String> adapterProf = new ArrayAdapter<>(getContext(),android.R.layout.simple_spinner_item, correlatori );
+        adapterProf.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        edtCorrelator.setAdapter(adapterProf);
+
         return view;
     }
 
@@ -127,11 +155,10 @@ public class InsertLaureaFragment extends Fragment {
 
     private void inserisciTesi() {
 
-
         String thesisName = edtThesisName.getText().toString();
         String mainSubject = edtMainSubject.getSelectedItem().toString();
         String estimatedTime = edtEstimatedTime.getText().toString();
-        String correlator = edtCorrelator.getText().toString();
+        String correlator = edtCorrelator.getSelectedItem().toString();
         String description = edtDescription.getText().toString();
         String thesisConstraints = edtThesisConstraints.getText().toString();
         String relatedProjects = edtRelatedProjects.getText().toString();
@@ -142,6 +169,10 @@ public class InsertLaureaFragment extends Fragment {
             tipoTesi = radioButtonSperimentale.getText().toString();
         }else if(radioButtonCompilativa.isChecked()){
             tipoTesi = radioButtonCompilativa.getText().toString();
+        }
+
+        if(correlator.equals("Nessuno")){
+            correlator="";
         }
 
         Map<String,String> infoTesi = new HashMap<>();
