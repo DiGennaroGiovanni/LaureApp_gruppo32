97ae3da5f20f23935f90863d3bdfea7574fdfa8c
Beppe Giacovelli <beppegiacovelli@gmail.com>
2023-01-15 21:10:28 +0100

    Modificati colori e temi delle activity (mancano i fragment)

--- app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222332/MainActivity.java
@@ -7,6 +7,7 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
