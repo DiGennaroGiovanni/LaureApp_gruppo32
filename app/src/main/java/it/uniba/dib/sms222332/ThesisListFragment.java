7515e8616a63c9769912787832369080006c9bd2
ProgettoSysAg <sysag2022@gmail.com>
2023-01-16 23:33:30 +0100

    Aggiunti bordi smussati, modifica cardTesi, aggiuntaPagina descrizione tesi

--- app/src/main/java/it/uniba/dib/sms222332/ThesisListFragment.java
+++ app/src/main/java/it/uniba/dib/sms222332/ThesisListFragment.java
@@ -10,11 +10,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,12 +33,16 @@ import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class ThesisListFragment extends Fragment {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth mAuth;
     FirebaseUser mUser;
     LinearLayout layout_lista_tesi;
+    LinearLayout info_tesi_layout;
+
 
     @Nullable
     @Override
@@ -45,6 +54,8 @@ public class ThesisListFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
         layout_lista_tesi = view.findViewById(R.id.layout_lista_tesi);
+
+
 
         db.collection("Tesi")
                 .get()
@@ -68,11 +79,13 @@ public class ThesisListFragment extends Fragment {
     private void addCardThesis(QueryDocumentSnapshot document) {
         View view = getLayoutInflater().inflate(R.layout.card_thesis, null);
 
+
         TextView txtName = view.findViewById(R.id.txtName);
         TextView txtType = view.findViewById(R.id.txtType);
         TextView txtDepartment = view.findViewById(R.id.txtDepartment);
         TextView txtTime = view.findViewById(R.id.txtTime);
         TextView txtCorrelator = view.findViewById(R.id.txtCorrelator);
+        Button infoTesiButton = view.findViewById(R.id.infoTesiButton);
 
         txtName.setText(document.getString("Name"));
         txtType.setText(document.getString("Type"));
@@ -80,6 +93,45 @@ public class ThesisListFragment extends Fragment {
         txtTime.setText(document.getString("Estimated Time"));
         txtCorrelator.setText(document.getString("Correlator"));
 
+
+
         layout_lista_tesi.addView(view);
+        infoTesiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                descrizioneTesi(document);
+
+            }
+        });
+    }
+
+    private Fragment descrizioneTesi(QueryDocumentSnapshot document) {
+        Bundle bundle = new Bundle();
+        bundle.putString("Thesis Name", document.getString("Name")); //TODO PASSARE IL RIFERIMENTO ALL'INTERO DOCUMENTO
+
+        ThesisDescription thesisDescription = new ThesisDescription();
+        thesisDescription.setArguments(bundle);
+
+        /*FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container, new ThesisDescription());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        */
+
+        return thesisDescription;
+    }
+
+    private void infoTesi() {
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container, new ThesisDescription());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
