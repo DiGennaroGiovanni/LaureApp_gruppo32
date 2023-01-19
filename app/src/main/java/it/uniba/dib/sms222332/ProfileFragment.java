bc350c6592b4d071b86535ef38c57264cc88fc79
Beppe Giacovelli <beppegiacovelli@gmail.com>
2023-01-19 11:35:50 +0100

    Completata pagina profilo ed eliminazione dei profili e schermata di logout

--- app/src/main/java/it/uniba/dib/sms222332/ProfileFragment.java
+++ app/src/main/java/it/uniba/dib/sms222332/ProfileFragment.java
@@ -1,27 +1,35 @@
 package it.uniba.dib.sms222332;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.util.Logger;
 
 
 public class ProfileFragment extends Fragment {
 
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser mUser = mAuth.getCurrentUser();
-//    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    TextView txtName, txtBadgeNumber, txtFaculty;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    TextView txtName, txtBadgeNumber, txtFaculty, txtEmail;
     Button btnDeleteProfile;
     String name, surname, badgeNumber, faculty, email;
 
@@ -39,31 +47,37 @@ public class ProfileFragment extends Fragment {
             faculty = bundle.getString("faculty");
             email = bundle.getString("email");
         }
-        email=MainActivity.account.getEmail();
 
         txtBadgeNumber = view.findViewById(R.id.txtBadgeNumber);
         txtName = view.findViewById(R.id.txtNameSurname);
         txtFaculty = view.findViewById(R.id.txtFaculty);
+        txtEmail = view.findViewById(R.id.txtEmail);
         btnDeleteProfile = view.findViewById(R.id.btnDeleteProfile);
 
         txtBadgeNumber.setText(badgeNumber);
-        txtName.setText(name +" "+ surname);
+        String nameSurname = name +" "+ surname;
+        txtName.setText(nameSurname);
         txtFaculty.setText(faculty);
+        txtEmail.setText(email);
 
-//        String userEmailId = mAuth.getCurrentUser().getEmail();
-//        DocumentReference documentReference = db.collection("studenti").document(userEmailId);
 
         //TODO Confermare scelta e rimandare ad activity conclusiva
-//        btnEliminaProfilo.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                documentReference.delete();
-//                mUser.delete();
-//                Intent intent = new Intent(getActivity(), LoginActivity.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-//                startActivity(intent);
-//            }
-//        });
+        btnDeleteProfile.setOnClickListener(view1 -> {
+
+            DocumentReference documentReference;
+
+            if (badgeNumber.equals(""))
+                documentReference = db.collection("professori").document(email);
+            else
+                documentReference = db.collection("studenti").document(email);
+
+            mUser.delete();
+            documentReference.delete();
+
+            Intent intent = new Intent(getActivity(), LogoutActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
         return view;
     }
 }
