6b95374a1df711fef2efd34d248b2f7c92b95707
Beppe Giacovelli <beppegiacovelli@gmail.com>
2023-01-21 12:52:17 +0100

    Implementata snackbar logout

--- app/src/main/java/it/uniba/dib/sms222332/LoginActivity.java
+++ app/src/main/java/it/uniba/dib/sms222332/LoginActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
@@ -24,12 +25,17 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText edtEmailLogin,edtPasswordLogin;
     private FirebaseAuth mAuth;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if(getIntent().getBooleanExtra("logout", false)){
+            View view = findViewById(android.R.id.content);
+            Snackbar.make(view, "Logout effettuato!", Snackbar.LENGTH_SHORT).show();
+        }
 
         // Dichiaro i pulsanti presenti nella schermata
         Button registerBtn = findViewById(R.id.btnRegistrati);
@@ -40,21 +46,13 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         // Passo alla schermata di registrazione
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegistratiActivity.class);
-                startActivity(registerIntent);
-            }
+        registerBtn.setOnClickListener(view -> {
+            Intent registerIntent = new Intent(LoginActivity.this, RegistratiActivity.class);
+            startActivity(registerIntent);
         });
 
         // Passo alla schermata home
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                performLogin();
-            }
-        });
+        loginBtn.setOnClickListener(view -> performLogin());
     }
 
     private void performLogin( ) {
@@ -70,90 +68,81 @@ public class LoginActivity extends AppCompatActivity {
             edtPasswordLogin.setError("Inserisci una password valida (deve avere almeno 6 caratteri)");
         }else{
 
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()){
 
-                        //VERIFICARE LA TIPOLOGIA DI ACCOUNT -> STUDENTE
+                    //VERIFICARE LA TIPOLOGIA DI ACCOUNT -> STUDENTE
 
-                        // Crea un riferimento alla raccolta studenti
-                        CollectionReference collectionReferenceStud = db.collection("studenti");
-                        // Crea un task per ottenere il documento con l'ID specifico
-                        Task<DocumentSnapshot> checkStud = collectionReferenceStud.document(email).get();
-                        // Aspetta che il task sia completato
-                        checkStud.addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    DocumentSnapshot document = task.getResult();
-                                    if (document.exists()) {
+                    // Crea un riferimento alla raccolta studenti
+                    CollectionReference collectionReferenceStud = db.collection("studenti");
+                    // Crea un task per ottenere il documento con l'ID specifico
+                    Task<DocumentSnapshot> checkStud = collectionReferenceStud.document(email).get();
+                    // Aspetta che il task sia completato
+                    checkStud.addOnCompleteListener(task12 -> {
+                        if (task12.isSuccessful()) {
+                            DocumentSnapshot document = task12.getResult();
+                            if (document.exists()) {
 
-                                        Map<String,Object> datiStudente =  document.getData();
+                                Map<String,Object> datiStudente =  document.getData();
 
 
-                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 
-                                        intent.putExtra("account_type",(String) datiStudente.get("Account Type"));
-                                        intent.putExtra("name",(String) datiStudente.get("Name"));
-                                        intent.putExtra("surname",(String) datiStudente.get("Surname"));
-                                        intent.putExtra("badge_number",(String) datiStudente.get("Badge Number"));
-                                        intent.putExtra("faculty",(String) datiStudente.get("Faculty"));
-                                        intent.putExtra("email", email);
+                                intent.putExtra("account_type",(String) datiStudente.get("Account Type"));
+                                intent.putExtra("name",(String) datiStudente.get("Name"));
+                                intent.putExtra("surname",(String) datiStudente.get("Surname"));
+                                intent.putExtra("badge_number",(String) datiStudente.get("Badge Number"));
+                                intent.putExtra("faculty",(String) datiStudente.get("Faculty"));
+                                intent.putExtra("email", email);
 
-                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(intent);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
 
-                                    } else {
+                            } else {
 
-                                        //VERIFICARE LA TIPOLOGIA DI ACCOUNT -> PROFESSORE
-                                        // Crea un riferimento alla raccolta studenti
-                                        CollectionReference collectionReferenceProf = db.collection("professori");
-                                        // Crea un task per ottenere il documento con l'ID specifico
-                                        Task<DocumentSnapshot> checkProf = collectionReferenceProf.document(email).get();
-                                        // Aspetta che il task sia completato
-                                        checkProf.addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                if (task.isSuccessful()) {
-                                                    DocumentSnapshot document = task.getResult();
-                                                    if (document.exists()) {
-                                                        Map<String,Object> datiProfessore =  document.getData();
+                                //VERIFICARE LA TIPOLOGIA DI ACCOUNT -> PROFESSORE
+                                // Crea un riferimento alla raccolta studenti
+                                CollectionReference collectionReferenceProf = db.collection("professori");
+                                // Crea un task per ottenere il documento con l'ID specifico
+                                Task<DocumentSnapshot> checkProf = collectionReferenceProf.document(email).get();
+                                // Aspetta che il task sia completato
+                                checkProf.addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        DocumentSnapshot document1 = task1.getResult();
+                                        if (document1.exists()) {
+                                            Map<String,Object> datiProfessore =  document1.getData();
 
-                                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 
-                                                        intent.putExtra("account_type",(String) datiProfessore.get("Account Type"));
-                                                        intent.putExtra("name",(String) datiProfessore.get("Name"));
-                                                        intent.putExtra("surname",(String) datiProfessore.get("Surname"));
-                                                        intent.putExtra("faculty",(String) datiProfessore.get("Faculty"));
-                                                        intent.putExtra("email", email);
+                                            intent.putExtra("account_type",(String) datiProfessore.get("Account Type"));
+                                            intent.putExtra("name",(String) datiProfessore.get("Name"));
+                                            intent.putExtra("surname",(String) datiProfessore.get("Surname"));
+                                            intent.putExtra("faculty",(String) datiProfessore.get("Faculty"));
+                                            intent.putExtra("email", email);
 
-                                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                                                        startActivity(intent);
-                                                    }
-                                                } else {
-                                                    // Errore durante la lettura del documento
-                                                }
-                                            }
-                                        });
-
-
-
-
-                                    }
-                                    } else {
-                                           // Errore durante la lettura del documento
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            startActivity(intent);
                                         }
+                                    } else {
+                                        // Errore durante la lettura del documento
                                     }
                                 });
-                        //----------------------------------------------------------------------------------------------------------------------
 
 
 
-                    }else{
 
-                        Toast.makeText(LoginActivity.this, "Utente non trovato, effettua la registrazione o inserisci correttamente i dati ", Toast.LENGTH_LONG).show();
-                    }
+                            }
+                            } else {
+                                   // Errore durante la lettura del documento
+                                }
+                            });
+                    //----------------------------------------------------------------------------------------------------------------------
+
+
+
+                }else{
+
+                    Toast.makeText(LoginActivity.this, "Utente non trovato, effettua la registrazione o inserisci correttamente i dati ", Toast.LENGTH_LONG).show();
                 }
             });
         }
