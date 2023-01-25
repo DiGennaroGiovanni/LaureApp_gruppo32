7483553717ffd50b4da3ba2eef83f34ccb39e202
FilippoLeone <filippo.leone@outlook.com>
2023-01-25 17:58:10 +0100

    Ultimato funzionamento tasto condivisione pdf su app terze parti e aggiunta, modifica ed eliminazione pdf tesi.

--- app/src/main/java/it/uniba/dib/sms222332/tools/PDFUtility.java
+++ app/src/main/java/it/uniba/dib/sms222332/tools/PDFUtility.java
@@ -2,12 +2,6 @@ package it.uniba.dib.sms222332.tools;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
@@ -15,7 +9,6 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
-import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -32,18 +25,17 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
+import java.io.OutputStream;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 
 import it.uniba.dib.sms222332.R;
 
 final public class PDFUtility {
+
     private static final String TAG = PDFUtility.class.getSimpleName();
     private static final Font FONT_TITLE = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
-    /*private static final Font FONT_SUBTITLE = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.NORMAL);*/
-
     private static final Font FONT_CELL = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
     private static final Font FONT_COLUMN = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.NORMAL);
 
@@ -51,13 +43,14 @@ final public class PDFUtility {
         void onPDFDocumentClose(File file);
     }
 
-    public static void createPdf(@NonNull Context mContext, OnDocumentClose mCallback, Map<String, String> items, @NonNull String title, boolean isPortrait, File pdfFile) throws Exception {
+    public static void createPdf(@NonNull Context mContext, OnDocumentClose mCallback, LinkedHashMap<String, String> items, boolean isPortrait, File pdfFile) throws Exception {
 
         Document document = new Document();
         document.setMargins(24f, 24f, 32f, 32f);
         document.setPageSize(isPortrait ? PageSize.A4 : PageSize.A4.rotate());
 
-        PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(pdfFile));
+        OutputStream outputStream = new FileOutputStream(pdfFile, false);
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, outputStream);
         pdfWriter.setFullCompression();
         pdfWriter.setPageEvent(new PageNumeration());
 
@@ -65,7 +58,7 @@ final public class PDFUtility {
 
         setMetaData(document);
 
-        addHeader(mContext, document, title);
+        addHeader(mContext, document);
         addEmptyLine(document, 3);
 
         document.add(createDataTable(items));
@@ -97,7 +90,7 @@ final public class PDFUtility {
         document.addHeader("DEVELOPER", "Gruppo Asse FGC");
     }
 
-    private static void addHeader(Context mContext, Document document, String title) throws Exception {
+    private static void addHeader(Context mContext, Document document) throws Exception {
         PdfPTable table = new PdfPTable(3);
         table.setWidthPercentage(100);
         table.setWidths(new float[]{2, 7, 2});
@@ -138,7 +131,7 @@ final public class PDFUtility {
             cell.setPaddingTop(25f);
             cell.setUseAscender(true);
 
-            Paragraph temp = new Paragraph("UNIBA - Summary thesis document", FONT_TITLE);
+            Paragraph temp = new Paragraph(mContext.getString(R.string.pdf_title), FONT_TITLE);
             temp.setAlignment(Element.ALIGN_CENTER);
             cell.addElement(temp);
 
@@ -167,7 +160,7 @@ final public class PDFUtility {
         document.add(table);
     }
 
-    private static PdfPTable createDataTable(Map<String, String> dataTable) throws DocumentException {
+    private static PdfPTable createDataTable(LinkedHashMap<String, String> dataTable) throws DocumentException {
         PdfPTable table1 = new PdfPTable(2);
         table1.setWidthPercentage(100);
         table1.setWidths(new float[]{1f, 2f});
@@ -183,7 +176,7 @@ final public class PDFUtility {
             cell.setPadding(4f);
             table1.addCell(cell);
 
-            cell = new PdfPCell(new Phrase("FILIPPO TESI", FONT_COLUMN));
+            cell = new PdfPCell(new Phrase(dataTable.get("Name"), FONT_COLUMN));
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setPadding(4f);
@@ -196,38 +189,46 @@ final public class PDFUtility {
 
         BaseColor lt_gray = new BaseColor(221, 221, 221); //#DDDDDD
         BaseColor cell_color;
+        dataTable.remove("Name");
 
-        for (Map.Entry<String, String> entry : dataTable.entrySet()) {
-            cell_color = alternate ? lt_gray : BaseColor.WHITE;
+        try {
+            for (LinkedHashMap.Entry<String, String> entry : dataTable.entrySet()) {
+                cell_color = alternate ? lt_gray : BaseColor.WHITE;
 
 
-            cell = new PdfPCell(new Phrase(entry.getKey(), FONT_CELL));
-            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setPaddingLeft(left_right_Padding);
-            cell.setPaddingRight(left_right_Padding);
-            cell.setPaddingTop(top_bottom_Padding);
-            cell.setPaddingBottom(top_bottom_Padding);
-            cell.setBackgroundColor(cell_color);
-            table1.addCell(cell);
+                cell = new PdfPCell(new Phrase(entry.getKey(), FONT_CELL));
+                cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setPaddingLeft(left_right_Padding);
+                cell.setPaddingRight(left_right_Padding);
+                cell.setPaddingTop(top_bottom_Padding);
+                cell.setPaddingBottom(top_bottom_Padding);
+                cell.setBackgroundColor(cell_color);
+                table1.addCell(cell);
 
-            cell = new PdfPCell(new Phrase(entry.getValue(), FONT_CELL));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setPaddingLeft(left_right_Padding);
-            cell.setPaddingRight(left_right_Padding);
-            cell.setPaddingTop(top_bottom_Padding);
-            cell.setPaddingBottom(top_bottom_Padding);
-            cell.setBackgroundColor(cell_color);
-            table1.addCell(cell);
+                if (entry.getValue().equals("")) cell = new PdfPCell(new Phrase("-", FONT_CELL));
+                else cell = new PdfPCell(new Phrase(entry.getValue(), FONT_CELL));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setPaddingLeft(left_right_Padding);
+                cell.setPaddingRight(left_right_Padding);
+                cell.setPaddingTop(top_bottom_Padding);
+                cell.setPaddingBottom(top_bottom_Padding);
+                cell.setBackgroundColor(cell_color);
+                table1.addCell(cell);
 
-            alternate = !alternate;
+                alternate = !alternate;
+            }
 
+            } catch (Exception e) {
+                Log.e("ERRORE", e.getMessage());
         }
+
+
         return table1;
     }
 
-    private static Image getImage(byte[] imageByte, boolean isTintingRequired) throws BadElementException, IOException {
+/*    private static Image getImage(byte[] imageByte, boolean isTintingRequired) throws BadElementException, IOException {
         Paint paint = new Paint();
         if (isTintingRequired) {
             paint.setColorFilter(new PorterDuffColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN));
@@ -242,6 +243,6 @@ final public class PDFUtility {
         Image image = Image.getInstance(stream.toByteArray());
         image.setWidthPercentage(80);
         return image;
-    }
+    }*/
 
 }
