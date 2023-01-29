0b713c06361b2ef34892a2b5fbe43f64724efdd4
Beppe Giacovelli <beppegiacovelli@gmail.com>
2023-01-29 13:52:11 +0100

    modificato metodo per ottenere il nome di un file e gestito il cambio di configurazione per la classe NewThesisFragment

--- app/src/main/java/it/uniba/dib/sms222332/professor/NewThesisFragment.java
+++ app/src/main/java/it/uniba/dib/sms222332/professor/NewThesisFragment.java
@@ -6,8 +6,10 @@ import static android.content.ContentValues.TAG;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +17,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -33,8 +35,6 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -57,23 +57,21 @@ public class NewThesisFragment extends Fragment {
 
 
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
-    EditText edtThesisName, edtEstimatedTime, edtDescription, edtMaterieRichieste, edtRelatedProjects, edtAverage;
+    EditText edtThesisName, edtEstimatedTime, edtDescription, edtRequestedExams, edtRelatedProjects, edtAverage;
+    RadioGroup radioGroup;
     RadioButton radioButtonSperimentale, radioButtonCompilativa;
-    Spinner edtMainSubject, edtCorrelator;
+    Spinner spinnerFaculty, spinnerCorrelator;
     Button addFile, buttonCreateThesis;
     TextView txtFaculty;
     //Istanze del database e del sistema di autenticazione di firebase
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    StorageReference storageReference;
-    FirebaseStorage storage;
-    Uri pdfUri;
-    ArrayList<Uri> files = new ArrayList<>();
+    Uri fileUri;
     LinearLayout layout;
 
-    CheckBox averageCheck, subjectCheck;
+    CheckBox averageCheck, examCheck;
 
-
-    ArrayList<String> correlatori = new ArrayList<>();
+    ArrayList<Uri> uris;
+    ArrayList<String> correlatori;
 
 
     @Override
@@ -82,29 +80,28 @@ public class NewThesisFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_new_thesis, container, false);
 
-        layout = view.findViewById(R.id.layout_lista);
+        uris = new ArrayList<>();
+        correlatori = new ArrayList<>();
 
+        layout = view.findViewById(R.id.layout_lista);
         averageCheck = view.findViewById(R.id.averageCheck);
-        subjectCheck = view.findViewById(R.id.requiredExamsCheck);
-        edtMaterieRichieste = view.findViewById(R.id.edtRequiredExams);
+        examCheck = view.findViewById(R.id.requiredExamsCheck);
+        edtRequestedExams = view.findViewById(R.id.edtRequiredExams);
         txtFaculty = view.findViewById(R.id.txtFaculty);
         edtAverage = view.findViewById(R.id.edtAverage);
         edtThesisName = view.findViewById(R.id.edtThesisName);
-        edtMainSubject = view.findViewById(R.id.edtMainSubject);
+        spinnerFaculty = view.findViewById(R.id.edtFaculty);
         edtEstimatedTime = view.findViewById(R.id.edtEstimatedTime);
-        edtCorrelator = view.findViewById(R.id.edtCorrelator);
+        spinnerCorrelator = view.findViewById(R.id.edtCorrelator);
         edtDescription = view.findViewById(R.id.edtDescription);
         edtRelatedProjects = view.findViewById(R.id.edtRelatedProjects);
+        radioGroup = view.findViewById(R.id.radioGroup);
         radioButtonSperimentale = view.findViewById(R.id.radioButtonSperimentale);
         radioButtonCompilativa = view.findViewById(R.id.radioButtonCompilativa);
         addFile = view.findViewById(R.id.addFile);
         buttonCreateThesis = view.findViewById(R.id.buttonCreateThesis);
 
-        edtMaterieRichieste.setEnabled(false);
-        edtAverage.setEnabled(false);
 
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
 
         /*      GESTISCO LO SPINNER     */
         // Creo un ArrayAdapter che contiene i valori dello spinner e gli affiso il layout
@@ -114,9 +111,20 @@ public class NewThesisFragment extends Fragment {
 
         //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Setto l'ArrayAdapter allo spinner
-        edtMainSubject.setAdapter(adapter);
-        buttonCreateThesis.setOnClickListener(view1 -> inserisciTesi());
+        spinnerFaculty.setAdapter(adapter);
 
+
+        if(savedInstanceState != null)
+            restoreInstance(savedInstanceState);
+
+        if(!examCheck.isChecked())
+            edtAverage.setEnabled(false);
+
+        if(!averageCheck.isChecked())
+            edtRequestedExams.setEnabled(false);
+
+
+        buttonCreateThesis.setOnClickListener(view1 -> inserisciTesi());
 
         addFile.setOnClickListener(view12 -> addFileOnClick());
 
@@ -124,13 +132,15 @@ public class NewThesisFragment extends Fragment {
 
         collectionRef.get().addOnCompleteListener(this::getCorrelator);
 
-
         averageCheck.setOnCheckedChangeListener((compoundButton, b) -> edtAverage.setEnabled(averageCheck.isChecked()));
 
-        subjectCheck.setOnCheckedChangeListener((buttonView, isChecked) -> edtMaterieRichieste.setEnabled(subjectCheck.isChecked()));
+        examCheck.setOnCheckedChangeListener((buttonView, isChecked) -> edtRequestedExams.setEnabled(examCheck.isChecked()));
+
+
 
         return view;
     }
+
 
     private void getCorrelator(Task<QuerySnapshot> task) {
         if (task.isSuccessful()) {
@@ -146,7 +156,7 @@ public class NewThesisFragment extends Fragment {
 
             ArrayAdapter<String> adapterProf = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, correlatori);
             adapterProf.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            edtCorrelator.setAdapter(adapterProf);
+            spinnerCorrelator.setAdapter(adapterProf);
 
         } else {
             Log.d(TAG, String.valueOf(R.string.error_documents), task.getException());
@@ -163,7 +173,7 @@ public class NewThesisFragment extends Fragment {
                     MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
         } else {
             // permesso già concesso, procedi con la lettura dei file
-            caricaFile();
+            takeFile();
         }
     }
 
@@ -171,9 +181,9 @@ public class NewThesisFragment extends Fragment {
     private void inserisciTesi() {
 
         String thesisName = edtThesisName.getText().toString();
-        String mainSubject = edtMainSubject.getSelectedItem().toString();
+        String mainSubject = spinnerFaculty.getSelectedItem().toString();
         String estimatedTime = edtEstimatedTime.getText().toString();
-        String correlator = edtCorrelator.getSelectedItem().toString();
+        String correlator = spinnerCorrelator.getSelectedItem().toString();
         String description = edtDescription.getText().toString();
         String materieRichieste = "";
         String mediaVoti = "";
@@ -182,18 +192,23 @@ public class NewThesisFragment extends Fragment {
         String professore = MainActivity.account.getEmail();
         int numeroIntero;
 
-
-        if (radioButtonSperimentale.isChecked()) {
-            tipoTesi = radioButtonSperimentale.getText().toString();
-        } else if (radioButtonCompilativa.isChecked()) {
-            tipoTesi = radioButtonCompilativa.getText().toString();
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.radioButtonSperimentale:
+                tipoTesi = radioButtonSperimentale.getText().toString();
+                break;
+            case R.id.radioButtonCompilativa:
+                tipoTesi = radioButtonCompilativa.getText().toString();
+                break;
+            default:
+                // Non è selezionato alcun RadioButton
+                break;
         }
 
-        if (subjectCheck.isChecked()) {
-            materieRichieste = edtMaterieRichieste.getText().toString();
+        if (examCheck.isChecked()) {
+            materieRichieste = edtRequestedExams.getText().toString();
 
             if (materieRichieste.isEmpty())
-                edtMaterieRichieste.setError(getString(R.string.required_subjects));
+                edtRequestedExams.setError(getString(R.string.required_subjects));
         }
 
         if (averageCheck.isChecked()) {
@@ -222,8 +237,24 @@ public class NewThesisFragment extends Fragment {
         infoTesi.put("Related Projects", relatedProjects);
         infoTesi.put("Required Exam", materieRichieste);
         infoTesi.put("Average", mediaVoti);
-        infoTesi.put("Type", tipoTesi);
         infoTesi.put("Student", "");
+
+        switch(tipoTesi){
+            case "Sperimentale":
+            case "Experimental":
+                infoTesi.put("Type", "Experimental");
+                break;
+
+            case "Compilativa":
+            case "Drafted":
+                infoTesi.put("Type", "Drafted");
+                break;
+
+            default:
+                break;
+        }
+
+
 
         if (thesisName.isEmpty()) {
             edtThesisName.setError(getString(R.string.enter_thesis_name));
@@ -233,7 +264,7 @@ public class NewThesisFragment extends Fragment {
             edtDescription.setError(getString(R.string.enter_thesis_description));
         } else {
             db.collection("Tesi").document(thesisName).set(infoTesi);
-            for (Uri file : files) {
+            for (Uri file : uris) {
                 uploadFile(file);
             }
 
@@ -259,21 +290,34 @@ public class NewThesisFragment extends Fragment {
         }
     }
 
-    private void addMaterialItem(String fileName, Uri fileUri) {
+    private void addMaterialItem(Uri fileUri) {
         View view = getLayoutInflater().inflate(R.layout.card_material, null);
+
+        String fileName = getNameFromUri(fileUri);
+
         TextView nameView = view.findViewById(R.id.materialName);
         Button delete = view.findViewById(R.id.deleteMaterial);
-        files.add(fileUri);
         nameView.setText(fileName);
 
         delete.setOnClickListener(v -> {
             layout.removeView(view);
-            files.remove(fileUri);
+            uris.remove(fileUri);
         });
         layout.addView(view);
     }
 
-    private void caricaFile() {
+    private String getNameFromUri(Uri pdfUri) {
+        String fileName = null;
+        Cursor cursor = requireActivity().getContentResolver().query(pdfUri, null, null, null, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            fileName = cursor.getString(nameIndex);
+            cursor.close();
+        }
+        return fileName;
+    }
+
+    private void takeFile() {
         Intent intent = new Intent();
         intent.setType("*/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -284,11 +328,9 @@ public class NewThesisFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 86 && resultCode == RESULT_OK && data != null) { //CONDIZIONE PER IL CARICAMENTO DEL PDF
-            pdfUri = data.getData();
-            File file = new File(pdfUri.getPath());
-            String fileName = file.getName();
-            addMaterialItem(fileName, pdfUri);
-
+            fileUri = data.getData();
+            addMaterialItem(fileUri);
+            uris.add(fileUri);
         }
     }
 
@@ -296,7 +338,7 @@ public class NewThesisFragment extends Fragment {
         // Creazione del riferimento al file sul server di Firebase
         File file = new File(uri.getPath());
         String pdfName = file.getName();
-        storageReference = FirebaseStorage.getInstance().getReference(edtThesisName.getText().toString() + "/" + pdfName);
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference(edtThesisName.getText().toString() + "/" + pdfName);
         // Caricamento del file sul server
         storageReference.putFile(uri)
                 .addOnSuccessListener(taskSnapshot -> {
@@ -308,7 +350,7 @@ public class NewThesisFragment extends Fragment {
     private void uploadPDF(Uri uriPDF) {
         File filePDF = new File(uriPDF.getPath());
         String pdfName = filePDF.getName();
-        storageReference = FirebaseStorage.getInstance().getReference("PDF_tesi" + "/" + pdfName);
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("PDF_tesi" + "/" + pdfName);
         // Caricamento del file sul server
         storageReference.putFile(uriPDF);
     }
@@ -316,8 +358,59 @@ public class NewThesisFragment extends Fragment {
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putString("thesis_name", edtThesisName.getText().toString());
+        outState.putString("description", edtDescription.getText().toString());
+        outState.putString("projects", edtRelatedProjects.getText().toString());
 
+        int selectedRadioBtnId = radioGroup.getCheckedRadioButtonId();
+        outState.putInt("selected_id", selectedRadioBtnId);
 
+        int facultyPosition = spinnerFaculty.getSelectedItemPosition();
+        outState.putInt("faculty", facultyPosition);
+        int correlatorPosition = spinnerCorrelator.getSelectedItemPosition();
+        outState.putInt("correlator", correlatorPosition);
+
+        if(!edtEstimatedTime.getText().toString().equals(""))
+            outState.putString("estimated_time", edtEstimatedTime.getText().toString());
+
+        outState.putBoolean("avg_check", averageCheck.isChecked());
+        if(averageCheck.isChecked()) {
+            outState.putString("avg_value", edtAverage.getText().toString());
+        }
+        outState.putBoolean("exams_check", examCheck.isChecked());
+        if(examCheck.isChecked()) {
+            outState.putString("exams_value", edtRequestedExams.getText().toString());
+        }
+
+        outState.putParcelableArrayList("uris",uris);
+
+    }
+
+    private void restoreInstance(Bundle bundle) {
+        edtThesisName.setText(bundle.getString("thesis_name", ""));
+        edtDescription.setText(bundle.getString("description", ""));
+        edtRelatedProjects.setText(bundle.getString("projects", ""));
+
+        radioGroup.check(bundle.getInt("selected_id"));
+
+        spinnerFaculty.setSelection(bundle.getInt("faculty", 0));
+        spinnerCorrelator.setSelection(bundle.getInt("correlator", 0));
+
+        edtEstimatedTime.setText(bundle.getString("estimated time", ""));
+
+        averageCheck.setChecked(bundle.getBoolean("avg_check"));
+        if (averageCheck.isChecked())
+            edtAverage.setText(bundle.getString("avg_value", ""));
+
+        examCheck.setChecked(bundle.getBoolean("exams_check"));
+        if(examCheck.isChecked())
+            edtRequestedExams.setText(bundle.getString("exams_value"));
+
+        uris = bundle.getParcelableArrayList("uris");
+        if (uris != null){
+            for(Uri uri : uris)
+                addMaterialItem(uri);
+        }
 
     }
 }
